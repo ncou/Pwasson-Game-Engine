f89246115202688e116b7930146bd6636fe73a77
SkyzohKey <skyzohkey@protonmail.com>
2016-08-11 18:25:20 +0200

    added rectangle to rectangle collisions

--- src/core/physics.js
+++ src/core/physics.js
@@ -3,6 +3,17 @@
 **/
 var Physics = {};
 
+/**
+* @enum CollisionDirection - The possible collision directions the CollisionSolver can use.
+**/
+Physics.CollisionDirection = {
+  NONE: -1,
+  TOP: 0,
+  LEFT: 1,
+  RIGHT: 2,
+  DOWN: 3,
+};
+
 // Export Physics as Game.Physics
 Game.Physics = Physics;
 
--- src/core/physics/collisionsolver.js
+++ src/core/physics/collisionsolver.js
@@ -12,7 +12,59 @@ function CollisionSolver () {
 * @return {bool} Returns true if both hitbox collides, false if not.
 **/
 CollisionSolver.prototype.RectangleRectangle = function (shape1, shape2) {
-  return true;
+  var collisionDirection = Game.Physics.CollisionDirection.NONE;
+
+  // Get the vector to check against.
+  var vX = (shape1.position.x + (shape1.size.x / 2)) - (shape2.position.x + (shape2.size.x / 2));
+  var vY = (shape1.position.y + (shape1.size.y / 2)) - (shape2.position.y + (shape2.size.y / 2));
+
+  // Add the half widths and half heights of the shapes.
+  var hWidths = (shape1.size.x / 2) + (shape2.size.x / 2);
+  var hHeights = (shape1.size.y / 2) + (shape2.size.y / 2);
+
+  /**
+  * If the X and Y vectors are less than the half width or half height, they must be inside the shape.
+  * This means that we have a collision!
+  **/
+  if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+    // Figure out on which side the collision happened. (TOP, LEFT, RIGHT, BOTTOM)
+    var oX = hWidths - Math.abs(vX);
+    var oY = hHeights - Math.abs(vY);
+    
+    if (oX >= oY) {
+      if (vY > 0) {
+        collisionDirection = Game.Physics.CollisionDirection.TOP;
+        // If the collide method returns true, we apply the collision, else we just giveup.
+        if (Game.Engine.scene.collide(collisionDirection, shape1, shape2) {
+          shape1.position.y += oY;
+        }
+      } else {
+        collisionDirection = Game.Physics.CollisionDirection.BOTTOM;
+        // If the collide method returns true, we apply the collision, else we just giveup.
+        if (Game.Engine.scene.collide(collisionDirection, shape1, shape2) {
+          shape1.position.y -= oY;
+        }
+      }
+    } else {
+      if (vX > 0) {
+        collisionDirection = Game.Physics.CollisionDirection.LEFT;
+        // If the collide method returns true, we apply the collision, else we just giveup.
+        if (Game.Engine.scene.collide(collisionDirection, shape1, shape2) {
+          shape1.position.x += oX;
+        }
+      } else {
+        collisionDirection = Game.Physics.CollisionDirection.RIGHT;
+        // If the collide method returns true, we apply the collision, else we just giveup.
+        if (Game.Engine.scene.collide(collisionDirection, shape1, shape2) {
+          shape1.position.x -= oX;
+        }
+      }
+    }
+    
+    return true;
+  }
+
+  return false;
 };
 
 /**
