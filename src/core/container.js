34c2d0c274a54e327a02c7647a7f1d6464ac918f
SkyzohKey <skyzohkey@protonmail.com>
2016-09-01 11:24:11 +0200

    Update Container, starting to refactor some code to improve the overall performances/maintainability.

--- src/core/container.js
+++ src/core/container.js
@@ -1,5 +1,9 @@
 /**
 * @class Container - A simple object that can have childrens.
+*
+* @property {Array} childrens - The container childrens.
+* @property {Vector} position - The container position within the scene.
+* @property {Vector} size - The container size within the scene. A container bigger than the scene will needs a Camera.
 **/
 function Container () {
   this._className = 'Container';
@@ -7,16 +11,22 @@ function Container () {
   this.childrens = [];
   this.position = new Game.Vector(x, y);
   this.size = new Game.Vector(width, height);
+  /**
+  * TODO: Implement Camera.
+  * The Camera must have a setTarget method that will set the tracked children the camera has to follow.
+  **/
+  // this.camera = new Game.Camera();
+  // this.camera.setTarget(this.childrens[0]);
 }
 
 /**
-* @public {int|bool} addChild - Add a children to this.
+* @public {Number|Boolean} addChild - Add a children to this.
 * @param {Sprite} child - The children sprite to add.
 * @return Returns the child index if succeed, false if not.
 **/
 Container.prototype.addChild = function (child) {
-  if (typeof child !== 'object') throw new Error('Child is not an Object.');
-  if (child._className != 'Sprite') throw new Error('Child is not a Sprite.');
+  if (child instanceof Object === false) throw new Error('Child is not an Object.');
+  if (child instanceof Game.Sprite === false) throw new Error('Child is not (extended from) a Sprite.');
 
   var index = this.childrens.push(child);
   if (this.childrens[index] !== child) return false; // There was an error while pushing the object.
@@ -28,20 +38,20 @@ Container.prototype.addChild = function (child) {
 };
 
 /**
-* @public {bool} removeChild - Removes a children from this.
-* @public {Sprite|int} child - The children, can be an instance of Sprite, or the children index in this.
+* @public {Boolean} removeChild - Removes a children from this.
+* @public {Sprite|Number} child - The children, can be an instance of Sprite, or the children index in this.
 **/
 Container.prototype.removeChild  = function (child) {
-  if (child._className === 'Sprite') { // Here we handle the case in which `child` is a Sprite.
+  if (child instanceof Game.Sprite) { // Here we handle the case in which `child` is a Sprite (or extended class from Sprite).
     if (this.childrens[child._index] === undefined) return false;
     this.childrens.splice(child._index, 1);
     if (this.childrens[child._index] === undefined) return true;
-  } else if (typeof child === 'number') { // Here we handle the case in which `child` is a Number.
+  } else if (child instanceof Number) { // Here we handle the case in which `child` is a Number (the child index).
     if (this.childrens[child] === undefined) return false;
     this.childrens.splice(child, 1);
     if (this.childrens[child] === undefined) return true;
   } else { // Error bro!
-    throw new Error('Invalid argument. Child must be a Sprite OR a Number.');
+    throw new Error('Invalid argument. Child must be a Sprite or a Number.');
     return false;
   }
 };
